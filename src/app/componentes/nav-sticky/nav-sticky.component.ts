f3ca5e15ab8f7f3c06ac1d633697010071109ab5
Douglasfj <douglasfj2004@gmail.com>
2023-04-17 17:42:07 -0300

    arrumando sticky nav - não finalizado

--- src/app/componentes/nav-sticky/nav-sticky.component.ts
+++ src/app/componentes/nav-sticky/nav-sticky.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class NavStickyComponent {
 
+  constructor() {
+    document.addEventListener('scroll', () => {
+      let nav: any = document.getElementById('nav')
+      console.log(window.innerHeight)
+      console.log(window.scrollY)
+      if (window.scrollY < window.innerHeight) {
+        
+        nav.classList.remove('active')
+        nav.classList.add('disabled')
+      } else if (window.scrollY >= window.innerHeight) {
+        
+        nav.classList.remove('disabled')
+      }
+      
+    })
+  }
+
+  btnNav(){
+    let nav: any = document.getElementById('nav')
+    if(nav.classList.contains('active')){
+      nav.classList.remove('active')
+    }else{
+      nav.classList.add('active')
+    }
+  }
+
 }
